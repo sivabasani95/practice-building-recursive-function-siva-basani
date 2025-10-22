9ddd56cb8823fb6743df102f60231e90bbad534c
sivabasani95 <sivabasani95@gmail.com>
2025-10-22 14:57:50 -0500

    Update reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -4,4 +4,7 @@ if (str.length <= 1) {
 }
 return str[str.length - 1] + reverseString(str. slice(0,-1));
 }
-console.log(reverseString("Hello","recursion","a",""));
+console.log(reverseString("hello"));
+console.log(reverseString("recursion")); 
+console.log(reverseString("a")); 
+console.log(reverseString("")); 
